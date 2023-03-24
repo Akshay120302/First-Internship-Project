6c24beb8441530c8cb6e22ef9781cbdfdf328de8
Akshay Sinha <108929978+Akshay120302@users.noreply.github.com>
2023-03-24 09:21:52 +0530

    Just before Externals of Internship 24/03/2023

---
+++ Accelerate/script.js
@@ -0,0 +1,67 @@
+let bar = document.getElementById('bar');
+let nav = document.getElementById('navbar');
+let close = document.getElementById('close');
+
+if(bar){
+    bar.addEventListener('click', () =>{
+        nav.classList.add('active');
+    })
+    close.addEventListener('click', () =>{
+        nav.classList.remove('active');
+    })
+}
+
+firstFrame = ()=>{
+    document.querySelectorAll('img')[4].src = "rahul.jpg"
+    document.querySelectorAll('img')[5].src = "akshay.jpg"
+    document.querySelectorAll('img')[6].src = "akshay3.jpg"
+
+
+    document.querySelectorAll('pre')[2].textContent = "Co-Founder \n Rahul Singh Rajput"
+    document.querySelectorAll('pre')[1].textContent = "Founder & C.E.O. \n Akshay Sinha"
+    document.querySelectorAll('pre')[0].textContent = "Co-Founder \n Anjali Singh"
+    
+}
+
+
+
+secondframe = ()=> {
+    document.querySelectorAll('img')[5].src = "akshay3.jpg"
+    document.querySelectorAll('img')[6].src = "rahul.jpg"
+    document.querySelectorAll('img')[4].src = "akshay.jpg"
+
+
+    document.querySelectorAll('pre')[2].textContent = "Founder & C.E.O. \n Akshay Sinha"
+    document.querySelectorAll('pre')[1].textContent = "Co-Founder \n Anjali Singh"
+    document.querySelectorAll('pre')[0].textContent = "Co-Founder \n Rahul Singh Rajput"
+
+}
+
+
+
+
+thirdframe = ()=> {
+    document.querySelectorAll('img')[6].src = "akshay.jpg"
+    document.querySelectorAll('img')[4].src = "akshay3.jpg"
+    document.querySelectorAll('img')[5].src = "rahul.jpg"
+
+
+    document.querySelectorAll('pre')[2].textContent = "Co-Founder \n Anjali Singh"
+    document.querySelectorAll('pre')[1].textContent = "Co-Founder \n Rahul Singh Rajput"
+    document.querySelectorAll('pre')[0].textContent = "Founder & C.E.O. \n Akshay Sinha"
+
+}
+
+
+
+FinalLoop = ()=>{
+    setTimeout(firstFrame,0000);
+    setTimeout(secondframe,4000);
+    setTimeout(thirdframe,8000);
+}
+
+setTimeout(FinalLoop,0000);
+
+setInterval(FinalLoop , 12000);
+
+
